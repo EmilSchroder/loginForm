7498d68f2047b7a5c8a934f4afc7aa63d9e748de
EmilSchroder <eschro66@gmail.com>
2018-07-31 13:24:23 +1200

    login checks if fields are blank

--- main.js
+++ main.js
@@ -3,9 +3,21 @@ var profile = {
     password1: "asdf"
 };
 
-function verify(){
+
+
+function read(){
     let uName = document.getElementsByName('username')[0].value;
     let pWord = document.getElementsByName('password')[0].value;
 
-    document.getElementById('testerText').innerHTML = 'something';
+    document.getElementById('testerText').innerHTML = verify(uName,pWord);
+
+    
+}
+
+function verify(uName,pWord){
+    if (uName===""){
+        return("<strong>Please enter a username<strong>");
+    } else if (pWord===""){
+        return("<strong>Please enter a password<strong>");
+    }
 }
