1e1c13349e1fa58a04dbeac87618002dc5f95740
EmilSchroder <eschro66@gmail.com>
2018-07-31 13:00:50 +1200

    js to look at values in input fields

---
+++ main.js
@@ -0,0 +1,11 @@
+var profile = {
+    username1: "test1",
+    password1: "asdf"
+};
+
+function verify(){
+    let uName = document.getElementsByName('username')[0].value;
+    let pWord = document.getElementsByName('password')[0].value;
+
+    document.getElementById('testerText').innerHTML = 'something';
+}
